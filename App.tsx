9de76a6e889dda84611b2216085d984870bcbf88
Diperios <98982092+Diperios@users.noreply.github.com>
2023-04-10 06:20:25 +0300

    1 file modified

--- App.tsx
+++ App.tsx
@@ -9,12 +9,10 @@ export default function App() {
     <div 
     style={{
         position: 'absolute',
-         left: '50%',
-         top: '30%',
-        transform: 'translate(-50%, -50%)'
-    }}
-    >
-    
+        left: '50%',
+        top: '30%',
+        transform: 'translate(-50%, -50%)',
+    }}>
       <h1>Tic Tac Toe</h1>
       <p>this is a tic tac toe game</p>
       <Board />
